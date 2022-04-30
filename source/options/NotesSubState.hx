23c070eafb0fbe7f6cc666e9b82e0af2a488cf47
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-04-30 13:06:24 -0300

    update NotesSubState.hx

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -91,6 +91,10 @@ class NotesSubState extends MusicBeatSubstate
 		add(hsbText);
 
 		changeSelection();
+		
+		#if android
+		addVirtualPad(FULL, A_B);
+		#end
 	}
 
 	var changingNote:Bool = false;
