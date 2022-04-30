100428445566d60e5589413cafa844a486e111c7
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-04-30 08:46:08 -0300

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -32,6 +32,8 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		
+		SUtil.gameCrashCheck();
 
 		if (stage != null)
 		{
@@ -67,20 +69,18 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if !debug
-		initialState = TitleState;
-		#end
+		SUtil.doTheCheck();
 
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
+
 
 		#if html5
 		FlxG.autoPause = false;
